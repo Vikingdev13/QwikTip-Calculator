ef54535a0e6be05fe61310a200627c613ed7c3cc
Vikingdev13 <vikingdeveloper13@gmail.com>
2019-09-30 15:56:10 -0400

    Initial Commit

---
+++ QwikTip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QwikTip Calculator
+//
+//  Created by John Piccione on 9/30/19.
+//  Copyright © 2019 John Piccione. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
